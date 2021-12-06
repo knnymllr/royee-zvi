6d18623b614d907467de631fa1c1f89d35024731
Kenny Miller <knnthrmllr@gmail.com>
2021-12-06 10:19:54 -0500

    cms engage

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,44 @@
 const CleanCSS = require("clean-css");
 const { minify } = require("terser");
+const markdownIt = require("markdown-it");
 
 module.exports = function (eleventyConfig) {
-    
-    eleventyConfig.addFilter("cssmin", function(code) {
-        return new CleanCSS({}).minify(code).styles;
-      });
 
-    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
-        code,
-        callback
-      ) {
-        try {
-          const minified = await minify(code);
-          callback(null, minified.code);
-        } catch (err) {
-          console.error("Terser error: ", err);
-          // Fail gracefully.
-          callback(null, code);
-        }
-      });
+  const md = new markdownIt({
+    html: true,
+  });
 
-    eleventyConfig.addPassthroughCopy("_includes/src/css");
-    eleventyConfig.addPassthroughCopy("_includes/src/js");
-    eleventyConfig.addPassthroughCopy("src/img");
-    eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addFilter("markdown", (content) => {
+    return md.render(content);
+  });
 
-    return {
-        dir: {
-            input: "src"
-        }
-    };
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      // Fail gracefully.
+      callback(null, code);
+    }
+  });
+
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("_includes/src/css");
+  eleventyConfig.addPassthroughCopy("_includes/src/js");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+
+  return {
+    dir: {
+      input: "src"
+    }
+  };
 }
--- src/index.md
+++ src/index.md
@@ -2,3 +2,4 @@
 layout: layouts/index
 image: /img/home.webp
 ---
+
